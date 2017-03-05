3d96df33d750269cf94286aa4cc414a263d33792
Vigo0101 <az5248@hotmail.com>
2017-03-05 18:09:37 +0100

    Create ES9018K2M-serial.sh

---
+++ Audiophonics/ES9018K2M/ES9018K2M-serial.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Audiophonics ES9018K2M DAC hardware volume management starting script for Volumio 2
+# Author: Jean-Charles BARBAUD
+#
+python /volumio/app/plugins/system_controller/i2s_dacs/scripts/ES9018K2M_serial_sync_volumio2.py
